7ae4b8682eb878d34c93ac249f1c79bf45f1f2f2
Charlie <carlossalazar1307@gmail.com>
2016-04-24 17:04:43 -0600

    creando proyecto

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clienteIntercambealoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the clienteIntercambealoApp
+ */
+angular.module('clienteIntercambealoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clienteIntercambealoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the clienteIntercambealoApp
+ */
+angular.module('clienteIntercambealoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
